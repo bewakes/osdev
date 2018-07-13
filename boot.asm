0603111c8588e606fc65a7a7e2e0ab6320cbd1cb
bewakes <spirit_bibek@yahoo.com>
2018-07-13 17:17:58 +0545

    Initial Boot loader: write message using BIOS

---
+++ boot.asm
@@ -0,0 +1,17 @@
+    mov ax, 0x07c0
+    mov ds, ax
+    mov si, msg
+ch_loop:
+    lodsb
+    or al, al
+    jz hang
+    mov ah, 0x0E
+    int 0x10
+    jmp ch_loop
+hang:
+    cli
+    hlt
+msg     db 'Hello world', 13, 10, 0
+    times 510-($-$$) db 0
+    db 0x55
+    db 0xAA
